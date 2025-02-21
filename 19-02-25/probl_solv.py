b84730b89574af18a90a0bb967709950855b20cd
kudumulaveeraharsha <137029402+kudumulaveeraharsha@users.noreply.github.com>
2025-02-21 13:49:20 +0530

    Add files via upload

---
+++ 19-02-25/probl_solv.py
@@ -0,0 +1,25 @@
+# num1=int(input("enter the number"))
+
+# if num1>0:
+#     print("positive number")
+# elif num1==0:
+#     print("Zero")
+# else: 
+#     print("negitive number")
+
+
+# num1=int(int(enter a age))
+
+# num1=10
+# if num1%2==0:
+#     print("even")
+# # else:
+# #     print("odd")
+
+# time=12
+# str1="good moring" if time<12 else "good afternoon"
+# print(str1)
+
+operation=int("Enter operation to perform").lower
+num1=float("Enter first num")
+num2=float("Enter secound number")
